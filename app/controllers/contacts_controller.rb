071a450d634517135e015b4a234514916a49b71d
N <limepassion@gmail.com>
2017-06-13 17:13:39 +0300

    5

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,6 +2,13 @@ class ContactsController < ApplicationController
   def new
   end
   def create
-      render plain: params[:contact].inspect
+     # render plain: params[:contact].inspect
+    @contact = Contact.new(contact_params)
+    @contact.save
+  end
+  private
+
+  def contact_params
+    params.require(:contact).permit(:email, :message)
   end
 end
